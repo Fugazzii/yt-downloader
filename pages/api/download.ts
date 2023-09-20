09fa83b00b8fd063f2af0a6bdc60ae3347ef26d4
Fugazzii <sichinavailia@gmail.com>
2023-09-20 23:33:31 +0400

    Initial

---
+++ pages/api/download.ts
@@ -0,0 +1,37 @@
+import fs from "fs";
+import ytdl from "ytdl-core";
+import path from "path";
+import os from "os";
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  data: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  
+  try {
+    const { url, id } = req.body;
+    const info = await ytdl.getInfo(id);
+    const { title } = info.videoDetails;
+  
+    ytdl(
+      url, 
+      { quality: "highest", filter: "audioandvideo" }
+    ).pipe(res);
+    
+    const filePath = path.join(os.homedir(), "Downloads", `${title}.mp4`);
+    const writeStream = fs.createWriteStream(filePath);
+    writeStream.on('finish', () => {
+      console.log('Video downloaded successfully!');
+    });
+    ytdl(url, { quality: 'highest', filter: "audioandvideo" }).pipe(writeStream);
+    res.status(200).json({ data: url });      
+  } catch (error) {
+    res.status(500).json({ data: `Error is:  + ${error}`});
+  }
+}
