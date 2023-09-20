c9b21ce5c0ad48f15da66ab80a228ef1443cf413
Fugazzii <sichinavailia@gmail.com>
2023-09-20 23:35:04 +0400

    Implemented download hook

---
+++ hooks/useDownload.ts
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+interface Props {
+    url: string,
+    id: string
+};
+
+export default function useDownload({ url, id }: Props) {
+    const [downloading, setDownloading] = useState<boolean>(false);
+    const [downloaded, setDownloaded] = useState<boolean>(false);
+    const [error, setError] = useState<any>(null);
+
+    async function handleDownload() {
+        try {
+            setDownloaded(false);
+            setDownloading(true);
+            const response = await fetch("http://localhost:3000/api/download", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ url, id })
+            });
+            
+            if(!response.ok) throw new Error("Failed to download");
+
+            setDownloaded(true);
+            setDownloading(false);
+        } catch (error) {
+            setError(error);
+        }
+    }
+
+    function reset() {
+        setDownloaded(false);
+        setDownloading(false);
+        setError(null);
+    }
+
+    return { downloaded, downloading, error, handleDownload, reset };
+}
